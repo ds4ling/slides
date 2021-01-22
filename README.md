5430bfa30ee1f4ca95ccf449c623fad5a3a51716
jvcasillas <joseph.casillas@rutgers.edu>
2021-01-22 08:31:27 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+ds4ling slides
+================
+
+1.  [01\_getting\_started](https://ds4ling.github.io/slides/01_getting_started/)
---
+++ assets/scripts/helpers.R
@@ -0,0 +1,31 @@
+# Packages --------------------------------------------------------------------
+
+# Custom
+library("ds4ling")
+library("lingStuff")
+library("untidydata")
+
+# Tidyverse
+library("dplyr")
+library("tidyr")
+library("readr")
+library("here")
+
+# Plotting
+library("ggplot2")
+library("patchwork")
+library("ggridges")
+library("ggrepel")
+library("viridis")
+library("ggfortify")
+
+# Misc
+library("knitr")
+library("kableExtra")
+library("latex2exp")
+library("broom")
+library("fGarch")
+library("RefManageR")
+
+# set seed for reproducibility
+set.seed(12345)
